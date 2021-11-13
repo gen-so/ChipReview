7049e3016cbb421256fe0738249ec49b14458c7c
Vignes <vignes@live.com.my>
2021-11-13 22:42:28 +0800

    1.5 - api updates

--- Api/GetReview.cs
+++ Api/GetReview.cs
@@ -6,32 +6,29 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
 namespace BlazorApp.Api
 {
-    public class ChipReviewApi
+    public class GetReview
     {
-        private readonly ILogger<ChipReviewApi> _logger;
+        private readonly ILogger<GetReview> _logger;
 
-        public ChipReviewApi(ILogger<ChipReviewApi> log)
+        public GetReview(ILogger<GetReview> log)
         {
             _logger = log;
         }
 
         [FunctionName("GetReview")]
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
-        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-
 
 
             return new OkObjectResult("Hello Wordl!");
